cd9f9e67462ef86a5328e111c99d01b3d53e48d0
Flavio <flavioluiscarneiro@gmail.com>
2024-07-20 11:57:28 -0300

    feat: adiciona logs nos serviços customer e room

--- src/main/java/br/com/mobicare/testcase/eclipsehotel/service/CustomerService.java
+++ src/main/java/br/com/mobicare/testcase/eclipsehotel/service/CustomerService.java
@@ -2,6 +2,8 @@ package br.com.mobicare.testcase.eclipsehotel.service;
 
 import br.com.mobicare.testcase.eclipsehotel.model.Customer;
 import br.com.mobicare.testcase.eclipsehotel.repository.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +12,28 @@ import java.util.Optional;
 
 @Service
 public class CustomerService {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
     @Autowired
     private CustomerRepository customerRepository;
 
     public List<Customer> findAll() {
+        logger.info("Procurando por todos os clientes");
         return customerRepository.findAll();
     }
 
     public Optional<Customer> findById(Long id) {
+        logger.info("Procurando pelo cliente de id " + id);
         return customerRepository.findById(id);
     }
 
     public Customer save(Customer customer) {
+        logger.info("Salvando as informações do cliente " + customer.getName());
         return customerRepository.save(customer);
     }
 
     public void deleteById(Long id) {
+        logger.info("Excluindo as informações do cliente de id " + id);
         customerRepository.deleteById(id);
     }
 }
--- src/main/java/br/com/mobicare/testcase/eclipsehotel/service/RoomService.java
+++ src/main/java/br/com/mobicare/testcase/eclipsehotel/service/RoomService.java
@@ -2,6 +2,8 @@ package br.com.mobicare.testcase.eclipsehotel.service;
 
 import br.com.mobicare.testcase.eclipsehotel.model.Room;
 import br.com.mobicare.testcase.eclipsehotel.repository.RoomRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,25 +11,30 @@ import java.util.List;
 
 @Service
 public class RoomService {
+    private static final Logger logger = LoggerFactory.getLogger(RoomService.class);
 
     @Autowired
     private RoomRepository roomRepository;
 
     public List<Room> findAll() {
+        logger.info("Procurando por todos os quartos");
         return roomRepository.findAll();
     }
 
     public Room findById(Long id) {
+        logger.info("Procurando pelo quarto de id " + id);
         return roomRepository.findById(id).orElse(null);
     }
 
     public Room save(Room room) {
+        logger.info("Salvando o registro do quarto de id " + room.getId());
         return roomRepository.save(room);
     }
 
     public Room update(Long id, Room roomDetails) {
         Room room = roomRepository.findById(id).orElse(null);
         if (room != null) {
+            logger.info("Atualizando o registro do quarto de id " + roomDetails.getId());
             room.setType(roomDetails.getType());
             room.setPrice(roomDetails.getPrice());
             return roomRepository.save(room);
@@ -36,6 +43,7 @@ public class RoomService {
     }
 
     public void delete(Long id) {
+        logger.info("Excluindo o registro do quarto de id " + id);
         roomRepository.deleteById(id);
     }
 }
